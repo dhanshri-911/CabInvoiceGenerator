6052f393047ce97b2e80d2a2d098986bfd90aeac
Dhanshri911 <dszingade911@gmail.com>
2022-04-18 10:08:32 +0530

    UC4:Invoice Service

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -4,11 +4,12 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-public class InvoiceGeneratorTest  {
+public class InvoiceGeneratorTest {
     public static InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-   @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare()  {
-        double distance  = 2.0;
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
@@ -16,12 +17,26 @@ public class InvoiceGeneratorTest  {
 
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-         Ride[] rides = {new Ride(2.0, 5),
-                 new Ride(0.1, 1)};
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
         InvoiceSummery summary = invoiceGenerator.calculateFareReturnObject(rides);
         InvoiceSummery expectedSummary = new InvoiceSummery(2, 30);
-        if(expectedSummary.getAverageFare() == summary.getAverageFare() && expectedSummary.getNumberOfRides() == summary.getNumberOfRides() && expectedSummary.getTotalFare() == summary.getTotalFare())
+        if (expectedSummary.getAverageFare() == summary.getAverageFare() && expectedSummary.getNumberOfRides() == summary.getNumberOfRides() && expectedSummary.getTotalFare() == summary.getTotalFare())
             Assert.assertEquals(1, 1);
     }
 
+    @Test
+    public void givenUserId_ShouldReturnInvoiceSummary() {
+        String userId = "User1";
+        InvoiceService invoiceService = new InvoiceService();
+        ArrayList<Ride> rides = new ArrayList<Ride>();
+        rides.add(new Ride(2.0, 5));
+        rides.add(new Ride(0.1, 1));
+        invoiceService.addRide(userId, rides);
+        ArrayList<Ride> listOfRides = invoiceService.getRides(userId);
+        InvoiceSummery summaryForUser1 = invoiceGenerator.calculateFareReturnObject(listOfRides.toArray(new Ride[0]));
+        InvoiceSummery expectedSummary = new InvoiceSummery(2, 30);
+        if (expectedSummary.getAverageFare() == summaryForUser1.getAverageFare() && expectedSummary.getNumberOfRides() == summaryForUser1.getNumberOfRides() && expectedSummary.getTotalFare() == summaryForUser1.getTotalFare())
+            Assert.assertEquals(1, 1);
+    }
 }
